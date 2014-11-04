819868ced7867f18d6f67099552a65436d004c81
chinbr <bc11375@hotmail.com>
2014-11-03 23:54:47 -0500

    Logout functionality
    Changed logout.html into logout.php. Logout.php deletes session and
    displays logout page. All log out links on site are changed from
    logout.html to logout.php

--- front_end/logout.php
+++ front_end/logout.php
@@ -1,3 +1,11 @@
+
+<?php
+    //automatically destroys user session data
+    session_start();
+    $_SESSION = array();
+    session_destroy();
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -6,6 +14,7 @@
 	</head>
 	<body>
 		<h1>You have successfully logged out!</h1>
+	
 		<p class="logoutLink"><a href="./loginpage/loginpage/login.html">Return to login page</a></p>
 		<p class="logoutLink"><a href="JavaScript:window.close()">Close Window</a></p>
 	</body>
