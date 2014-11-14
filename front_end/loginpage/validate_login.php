483f3cd2b386267064af5ef76cbe1b79faad221f
chinbr <bc11375@hotmail.com>
2014-11-13 20:23:10 -0500

    Support for DB.php changes
    login.html and validate_login.php updated

--- front_end/loginpage/validate_login.php
+++ front_end/loginpage/validate_login.php
@@ -6,8 +6,8 @@
 
 
 	try {
-		$dbconn = new DB();
-
+		//$dbconn = new DB();
+		$dbconn = DB::getInstance();
 		$empty = empty($_POST['user']) || empty($_POST['pass']); 
 		if( isset($_POST['user'], $_POST['pass']) && $empty == false) {
 
@@ -18,18 +18,29 @@
 			$hashpass = sha1($password . $salt); 	 //use hashpass once inserted correct pass
 			
 			
-			$query = $dbconn->query("SELECT * FROM (SELECT * FROM users) user, (SELECT * FROM passwords) pass WHERE user.email='$username' AND pass.userId = user.userId AND password='$password'");
+			$array = array(':email' => $username, ':password' => $password);
+			$db = $dbconn->prep_execute("SELECT * FROM (SELECT * FROM users) user, (SELECT * FROM passwords) pass WHERE user.email= :email AND pass.userId = user.userId AND password= :password", $array);
+	
 
-			if ($row = $query->fetch()) { //successfully logged in
+			$rowcount = sizeof($db);
+			if ($rowcount == 1) {
 				$_SESSION["user"] = $username;
-				//$_SESSION["email"] = $row['email'];
-				//$_SESSION["phone"] = $row['phone'];
-				echo "1";
-				exit();
+				
 			}
-			else {
-				echo "2";
-			}
+			echo $rowcount;
+			exit();
+			// //echo sizeof($db); //works
+			// //$query = $dbconn->query("SELECT * FROM (SELECT * FROM users) user, (SELECT * FROM passwords) pass WHERE user.email='$username' AND pass.userId = user.userId AND password='$password'");
+			// //if ($row = $query->fetch()) { //successfully logged in
+			// 	//$_SESSION["user"] = $username;
+			// 	//$_SESSION["email"] = $row['email'];
+			// 	//$_SESSION["phone"] = $row['phone'];
+			// 	//echo "1";
+			// 	exit();
+			// // }
+			// // else {
+			// // 	echo "2";
+			// // }
 		}
 		else {
 			echo "form not completed";
