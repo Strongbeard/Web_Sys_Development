000c81b6517801b6a2b2901d1942c0cd2e728690
Kevin <mahonk3@rpi.edu>
2014-11-19 16:51:26 -0500

    Added a TA to the database's populate.sql script. Doesn't have any classes assigned to him yet.

--- SQL/populate.sql
+++ SQL/populate.sql
@@ -1,10 +1,12 @@
 USE TA_Hunter;
 
 /* USERS */
-INSERT IGNORE INTO users (userid, email, isAdmin, isStudent, isTA, isTutor) VALUES (1,'mahonk3@rpi.edu',FALSE,TRUE,FALSE,FALSE);
+INSERT IGNORE INTO users (userid, email, isAdmin, isStudent, isTA, isTutor, firstName, lastName) VALUES (1,'mahonk3@rpi.edu',FALSE,TRUE,FALSE,FALSE,'Kevin','Mahon'); /* password = mahonk3 */
+INSERT IGNORE INTO users (userid, email, isAdmin, isStudent, isTA, isTutor, firstName, lastName) VALUES (2,'ta@test.com',FALSE,FALSE,TRUE,FALSE,'TA_First','TA_Last');
 
 /* PASSWORDS */
 INSERT IGNORE INTO passwords(userid, password) VALUES (1,'$2y$10$KN0.2dx2J7ZCj1aPqhruwuZ7YR0soWcv/rWX7FuURB2U9EC5wUe2u');
+INSERT IGNORE INTO passwords(userid, password) VALUES (2,'$2y$10$767S3Y8D7uui6wtKtrXJge7IgE8Wks0qq2MrpgRwGh8bkUTGfandy');
 
 /* COURSES */
 INSERT IGNORE INTO courses(name, subj, crse) VALUES ( 'BASIC DRAWING', 'ARTS', 1200 );
