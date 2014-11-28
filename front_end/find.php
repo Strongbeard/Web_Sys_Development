cf367c5bbb2a30817b4bfcf8a4293bf5c6cf6d59
chinbr <bc11375@hotmail.com>
2014-11-27 19:31:41 -0500

    keyword matching for search
    instead of using direct match, use LIKE keyword in mysql

--- front_end/find.php
+++ front_end/find.php
@@ -15,12 +15,12 @@ if (isset($_GET['ta_name']) && !empty($_GET['ta_name'])) {
 		//more than one name entered
 		if (count($name_arr) > 1) {
 			$search_arr = array(':firstName' => $name_arr[0], ':lastName' => $name_arr[1]);
-			$result = $dbconn->prep_execute("SELECT firstName, lastName, users.email, courses.subj AS subj, courses.crse AS crse, name FROM users, tas_courses, courses WHERE isTA = 1 AND firstName= :firstName AND lastName= :lastName AND users.email = tas_courses.email AND tas_courses.crse = courses.crse AND tas_courses.subj = courses.subj", $search_arr);
+			$result = $dbconn->prep_execute("SELECT firstName, lastName, users.email, courses.subj AS subj, courses.crse AS crse, name FROM users, tas_courses, courses WHERE isTA = 1 AND firstName LIKE CONCAT('%', :firstName,'%')  AND lastName LIKE CONCAT('%', :lastName,'%') AND users.email = tas_courses.email AND tas_courses.crse = courses.crse AND tas_courses.subj = courses.subj", $search_arr);
 			
 		}
 		else { //either first name or last name entered
 			$search_arr = array(':firstName' => $name_arr[0], ':lastName' => $name_arr[0]);
-			$result = $dbconn->prep_execute("SELECT firstName, lastName, users.email, courses.subj AS subj, courses.crse AS crse, name FROM users, tas_courses, courses WHERE isTA = 1 AND (firstName= :firstName OR lastName= :lastName) AND users.email = tas_courses.email AND tas_courses.crse = courses.crse AND tas_courses.subj = courses.subj", $search_arr);
+			$result = $dbconn->prep_execute("SELECT firstName, lastName, users.email, courses.subj AS subj, courses.crse AS crse, name FROM users, tas_courses, courses WHERE isTA = 1 AND (firstName LIKE CONCAT('%', :firstName,'%') OR lastName LIKE CONCAT('%', :lastName,'%')) AND users.email = tas_courses.email AND tas_courses.crse = courses.crse AND tas_courses.subj = courses.subj", $search_arr);
 	
 		}
 		//SELECT firstName, lastName, email, courses.subj, courses.crse, name FROM users, tas_courses, courses WHERE isTA = 1 and users.userId = tas_courses.userID and tas_courses.crse = courses.crse and tas_courses.subj = courses.subj 
@@ -54,7 +54,7 @@ if (isset($_GET['class_name']) && !empty($_GET['class_name'])) {
 		//school not selected
 		
 		$search_arr = array(':class_name' => $class_name);
-		$result = $dbconn->prep_execute("SELECT courses.subj, courses.crse, name, firstName, lastName, users.email FROM tas_courses, courses, users WHERE courses.name = :class_name AND tas_courses.subj = courses.subj AND tas_courses.crse = courses.crse AND tas_courses.email = users.email", $search_arr);
+		$result = $dbconn->prep_execute("SELECT courses.subj, courses.crse, name, firstName, lastName, users.email FROM tas_courses, courses, users WHERE courses.name LIKE CONCAT('%', :class_name,'%') AND tas_courses.subj = courses.subj AND tas_courses.crse = courses.crse AND tas_courses.email = users.email", $search_arr);
 			
 	
 		if ($result != null && sizeof($result) > 0) {
