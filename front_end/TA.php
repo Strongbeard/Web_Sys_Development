2824468deafb6ddcdbac42b8e7be34d5e89b5d46
Jonathan Yax <jonathan.yax@gmail.com>
2014-11-30 15:48:18 -0500

    Fixed the problem with viewing TA office hours

--- front_end/TA.php
+++ front_end/TA.php
@@ -7,46 +7,16 @@ require(SITE_ROOT . '/PHP/relations.php');
 $courses = COURSE::getAllCourses();
 $users = USER::getAllUsers();
 
-$message = '';
-$message_class = 'hidden';
-
 if( isset($_POST['form']) ) {
 	switch ($_POST['form']) {
 		case 'AddTAOfficeHours':
-			try {
-				$user = USER::fromDatabase($_POST['email']);
+/* 			try {
 				list($subj, $crse) = split('-', $_POST['course']);
-				if( $user->addTAOfficeHours($subj, intval($crse), $_POST['week_day'], $_POST['startTime'], $_POST['endTime']) ) {
-					$message = 'Success!';
-					$message_class = 'success';
-				}
-				else {
-					$message = 'ERROR: could not add TA office hours to database.';
-					$message_class = 'error';
-				}
+				$_SESSION['user']->addTAOfficeHours($subj, intval($crse), $_POST['week_day'], $_POST['startTime'], $_POST['endTime']);
 			}
 			catch( Exception $e ) {
-				$message = $e->getMessage();
-				$message_class = 'error';
 			}
-			break;
-		case 'DeleteTAOfficeHours':
-			list( $subj, $crse, $email, $week_day ) = split( ' ', $_POST['ta_hours'] );
-			try {
-				if( USER::fromDatabase($email)->removeTAOfficeHours( $subj, intval($crse), $week_day ) ) {
-					$message = 'Success!';
-					$message_class = 'success';
-				}
-				else {
-					$message = 'ERROR: could not delete TA office hours from database.';
-					$message_class = 'error';
-				}
-			}
-			catch( Exception $e ) {
-				$message = $e->getMessage();
-				$message_class = 'error';
-			}
-			break;
+			break; */
 	}		
 }
 ?>
@@ -103,7 +73,7 @@ if( isset($_POST['form']) ) {
 					<figure>
 					 <?php
 						$var = $_SESSION['user']->getFirstName();
-						echo "<p> " . $var . ": Add, View, or Delete your office hours! </p>" . "<br>";	
+						echo "<h2> Add, View, or Delete your office hours! </h2>" . "<br>";	
 					 ?>
 					
 					<h3>Add Your Office Hours</h3>
@@ -130,11 +100,11 @@ if( isset($_POST['form']) ) {
 							</div>
 							<div class="input_block">
 								<label for="AddTAOfficeHours_StartTime">Start Time</label>
-								<input id="AddTAOfficeHours_StartTime" type="time" name="startTime" />
+								<input id="AddTAOfficeHours_StartTime" type="time" name="startTime" required="required"/>
 							</div>
 							<div class="input_block">
 								<label for="AddTAOfficeHours_EndTime">End Time</label>
-								<input id="AddTAOfficeHours_EndTime" type="time" name="endTime" />
+								<input id="AddTAOfficeHours_EndTime" type="time" name="endTime" required="required"/>
 							</div>
 							<br>
 							<input class="input_block" type="submit" value="Add TA Office Hours" />
@@ -142,58 +112,46 @@ if( isset($_POST['form']) ) {
 					
 					<br><br>
 					<h3>View Your Office Hours</h3>
-					<?PHP
-					$TAemail = $_SESSION['user']->getEmail();
-					//connect to server
-					$connect = mysql_connect("localhost","root","");
-					
-					//connect to database
-					mysql_select_db("ta_hunter");
-					
-					//query the database
-					$query = mysql_query("SELECT * FROM ta_hours WHERE email = '$TAemail' ");
-					
-					//fetch the results/convert results into an array
-						WHILE($rows = mysql_fetch_array($query)):
-							$email = $rows['email'];
-							$subj = $rows['subj'];
-							$crse = $rows['crse'];
-							$week_day = $rows['week_day'];
-							$start_time = $rows['start_time'];
-							$end_time = $rows['end_time'];
+						<?PHP
+						$TAemail = $_SESSION['user']->getEmail();
+						//connect to server
+						$connect = mysql_connect("localhost","root","");
 						
-						echo "<table border=\"1\">
-						<tr>
-							<td><b>SUBJ</b></td>
-							<td><b>CRSE</b></td>
-							<td><b>Day</b></td>
-							<td><b>Start Time</b></td>
-							<td><b>End Time</b></td>
-						</tr>";
-						echo "<tr><td>$subj</td><td>$crse</td><td>$week_day</td><td>$start_time</td><td>$end_time</td></tr>";
-						endwhile;
-					
-					
-					?>
-					
-					
+						//connect to database
+						mysql_select_db("ta_hunter");
+						
+						//query the database
+						$query = mysql_query("SELECT * FROM ta_hours WHERE email = '$TAemail' ");
+						
+						//fetch the results/convert results into an array
+							WHILE($rows = mysql_fetch_array($query)):
+								$email = $rows['email'];
+								$subj = $rows['subj'];
+								$crse = $rows['crse'];
+								$week_day = $rows['week_day'];
+								$start_time = $rows['start_time'];
+								$end_time = $rows['end_time'];
+							
+							echo "<table border=\"1\">
+							<tr>
+								<td><b>SUBJ</b></td>
+								<td><b>CRSE</b></td>
+								<td><b>Day</b></td>
+								<td><b>Start Time</b></td>
+								<td><b>End Time</b></td>
+							</tr>";
+							echo "
+							<tr>
+								<td>$subj</td>
+								<td>$crse</td>
+								<td>$week_day</td>
+								<td>$start_time</td>
+								<td>$end_time</td>
+							</tr>";
+							echo "</table>";
+							endwhile;
+						?>
 					<br><br>
-					<!--
-					<h3>Delete Your Office Hours</h3>
-						<form id="DeleteTACourseRelation" action="#" method="POST">
-							<h2>Remove a TA's Course</h2>
-							<input type="hidden" name="form" value="DeleteTACourseRel" />
-							<div class="input_block">
-								<label for="DeleteTACourse_Rel">Course</label>
-								<select id="DeleteTACourse_Rel" name="ta_course" required>
-								<?php foreach(getAllTAsCourses() as $mappings) : ?>
-									<option value="<?php echo $mappings['course']->getSubj() . ' ' . $mappings['course']->getCrse() . ' ' . $mappings['user']->getEmail(); ?>"><?php echo $mappings['user']->getLastName() . ', ' . $mappings['user']->getFirstName() . ' (' . $mappings['user']->getEmail() . ') - ' . $mappings['course']->getSubj() . ' ' . $mappings['course']->getCrse() . ' ' . $mappings['course']->getName(); ?></option>
-								<?php endforeach; ?>
-								</select>
-							</div>
-							<input class="input_block" type="submit" value="Delete TA Course Relationship" />
-						</form>
-					-->
 					</figure>
 				
 				
